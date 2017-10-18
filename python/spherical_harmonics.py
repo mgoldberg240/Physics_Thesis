3008f4580fc453224c32890af8b46e55615db5b9
Matt Goldberg <mgoldbe2@bates.edu>
2017-10-18 17:50:35 -0400

    Still working on git commands.  This is the updated Sph Harm function as of October break.  Nearly perfect! See notes in comments.

--- python/spherical_harmonics.py
+++ python/spherical_harmonics.py
@@ -1,90 +1,145 @@
+# 10/11/17 signs are still off!
+# set up the code so that it accepts negative m.  Turns it positive, runs the code, outputs Yneg
+# Only need one outarg
+
 import numpy as np
 import cmath as c
+import scipy as scipy
+from scipy import special
 
-#user provides m,n,lambda, and theta
-N,M,LAM,THETA = input("Give n,m,lambda, and theta: ").split()
+#user provides m,n,theta, and lambda
+N,M,THETA,LAM = input("Give n,m,theta, and lambda: ").split()
 
 N = float(N)
-
-M = float(M)
-
-LAM = float(eval(LAM)) #evaluate numpy expressions, for example
-
+M = abs(float(M))
 THETA = float(eval(THETA)) #evaluate numpy expressions, for example
+LAM = float(eval(LAM)) #evaluate numpy expressions, for example
 
 
 #Spherical Harmonics - Complex Exponential Form
-def spherical_harmonics(n,m,lam,theta):
+def spherical_harmonics(n,m,theta,lam):
+	m = abs(m)
 	if n < m:
-		Y = 0
+		Y = float('NaN')
 		print("n must be greater than or equal to m")
 		return Y
 		quit()
-	Y_pos_m = c.exp(1j*m*lam) * Leg_poly(n,m,theta)
-	Y_neg_m = c.exp(-1j*m*lam) * Leg_poly(n,m,theta) * (-1)**m
-	#print("this is Y+, Y-",Y_pos_m,Y_neg_m)
 
-	# if m >= 0:
-	# 	Y = c.exp(1j*m*lam) * Leg_poly(n,m,np.cos(theta))
-	# else:
-	# 	Y = c.exp(-1j*m*lam) * Leg_poly(n,m,np.cos(theta)) * (-1)**m
-	#defined in one line by
-	# Y = ((1j)**(m+abs(m))) * ((np.math.factorial(n-abs(m))/np.math.factorial(n+abs(m)))**(0.5))*c.exp(1j*m*lam) * Leg_poly(n,m,theta)
-	
 
 	#normalization
-	normaliziation = norm_const(n,m)
-	Y_pos_m = normaliziation * Y_pos_m
-	Y_neg_m = normaliziation * Y_neg_m
-	return Y_pos_m,Y_neg_m
+	norm_const = normalize(n,m)
+	# Y_pos_m = c.exp(1j*m*lam) * Leg_poly(n,m,theta)
+	# Y_pos_m = normaliziation * Y_pos_m
 
-def norm_const(n,m):
-	K = np.sqrt( ((2*n+1)/(4*np.pi)) * (np.math.factorial(n-m)/np.math.factorial(n+m)) )
-	#K = K * ((-1)**m)*(dub_fact(2*n-1)/(2*m)) # fraudulent! achieved by guess and check
-	print("this is K: ",K)
+	if m >= 0:
+		Y = c.exp(1j*m*lam) * Leg_poly(n,m,theta) * norm_const
+	else:
+		Y = c.exp(-1j*m*lam) * Leg_poly(n,m,theta) * norm_const * (-1)**m
+	return Y
+
+
+	# if m != 0:
+	# 	Y_neg_m = c.exp(-1j*m*lam) * Leg_poly(n,m,theta) * (-1)**m
+	# 	Y_neg_m = norm_const * Y_neg_m
+	# else:
+	# 	Y_neg_m = "No Y_negative"
+	# return Y_pos_m,Y_neg_m
+
+
+# Normalization
+def normalize(n,m):
+	K = np.sqrt( ((2*n+1)/(4*np.pi)) * (np.math.factorial(n - m)/np.math.factorial(n + m)) )
+	K = K * dub_fact(2*M - 1) * (-1)**m # this is mysterious but it makes the code work!
 	return K
 
-# Associated Legendre Polynomials
-def Leg_poly(n,m,theta):
-	# if n == 0:
-	# 	P = 1
-	# elif n == 1:
-	# 	P = x
-	# else:
-	# 	P = (2*n-1)/n * x * Leg_poly(n-1,x) - (n-1)/n * Leg_poly(n-2,x)
-	# return P
-	P = ((np.sin(theta))**m) * Geg_poly(n-m,m,np.cos(theta))
-	#print("Leg_poly gives me P = ",P)
-	return P
-
-# Gegenbauer Polynomials - takes floating points n,m, and x
-# For spherical harmonics, associated LP is Geg_poly with m = 1/2
-def Geg_poly(n,m,x): #defined recursively! Notice that geg_poly calls itself
-	if m == 0: # used to be n == 1 --- something is up with recursion defn --- consult with gegC[] wolfram function
-		C = 1
-	elif n == 0:  #did this work??
-		C = 1
-	elif n == 1:
-		C = 2*m*x
-	else:
-		C = (1/n)*(2*x*(n+m-1)*Geg_poly(n-1,m,x) - (n+(2*m)-2)*Geg_poly(n-2,m,x))
-	print("Geg_poly:  Given n,m,x = ",n,m,x,", I output C = ",C)
-	return C
-
-
+# double factorial --- used for normalization...but why?
 def dub_fact(k):
 	if k <= 0:
 		return 1
 	else:
 		return k * dub_fact(k-2)
 
+# Associated Legendre Polynomials
+def Leg_poly(n,m,theta):
+	m_prime = m + 0.5 # adjust to definition in Boyd appendix A
+	P = ((np.sin(theta))**m) * Geg_poly(n-m,m_prime,np.cos(theta))
+	# print("Leg_poly gives me P = ",P) # used for testing
+	return P
+
+
+# Gegenbauer Polynomials - takes floating points n,m, and x
+def Geg_poly(n,m,x): #defined recursively!
+	if n == 0:
+		C = 1
+	elif n == 1:
+		C = 2*m*x
+	else:
+		C = (1/n)*( 2*x*(n+m-1)*Geg_poly(n-1,m,x) - (n+(2*m)-2)*Geg_poly(n-2,m,x) )
+	# print("Geg_poly:  Given n,m,x = ",n,m,x,", I output C = ",C) # used for testing
+	return C
 
 
 
 
-myY_pos,myY_neg = spherical_harmonics(N,M,LAM,THETA)
-print("Y(n,m) = ",myY_pos)
-print("Y(n,-m) = ",myY_neg)
+
+
+# myY = spherical_harmonics(N,M,THETA,LAM)
+# print("MY ~~ Y(n,m) = ",myY)
+# #print("Y(n,-m) = ",myY_neg)
+# theirY= scipy.special.sph_harm(M,N,LAM,THETA)
+# print("Their Y(n,m) = ",theirY)
+
+
+
+
+
+
+#################################################################
+#################################################################
+######## TESTING / TESTING / TESTING / TESTING / TESTING ########
+#################################################################
+#################################################################
+
+
+
+print("~~~~~~~~~~~~")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print("~~~~~~~~~~~~")
+
+# test a few values of my sph_harm vs scipy.special.sph_harm --- phi = lam
+LAM = np.pi/4
+THETA = np.pi/4
+success = 0
+iMax = 10
+for i in range(0,iMax):
+	M = -i
+	N = i + 2
+	myY = spherical_harmonics(N,M,THETA,LAM)
+	theirY = scipy.special.sph_harm(M,N,LAM,THETA)
+	print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-")
+	print("iteration ",i)
+	print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-")
+	print("N = ",N,", M = ",M,", THETA = ",THETA,", PHI = ",LAM)
+	print("mine: ",myY)
+	print("theirs: ",theirY)
+	if np.float32(myY.real) == np.float32(theirY.real) and np.float32(myY.imag) == np.float32(theirY.imag):
+		print("Success!")
+		success = success + 1
+print(" ")
+print('\033[1m' + str(success) + " out of " + str(iMax) + " tests were successful")
+print('\033[0m')
+
+
+
+
+
+
+
+
 
 
 # Notes for 10/9/17
@@ -92,4 +147,13 @@ print("Y(n,-m) = ",myY_neg)
 # n = 0, lambda free should give C = 1, but when using gegC[], it gives 0.  if instead you force lambda/m to be 1
 # then it makes C = 1 as desired --- this solves some problems - then there are some division by zero errors
 # does the addition of elif n == 0 work?
+
+# Notes for 10/10/17
+# check how far off the Y values are for n!=m --- I suspect it is something using dub fact
+# make it a conditional! if n!=m, weight by dubfact
+
+# Notes for 10/13/17
+# nearly there --- negatives arent perfect (try 1 -1 np.pi/3 np.pi/5)
+
+
 
